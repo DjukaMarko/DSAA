f29c7dc87da75066b950c0c4a003bc630f02dca1
Marko Djukic <marko.djukic.111@gmail.com>
2021-01-12 17:29:30 +0100

    Added Heap

--- src/datastructures/Heap.kt
+++ src/datastructures/Heap.kt
@@ -1,5 +1,7 @@
 package datastructures
-//complete
+
+import kotlin.properties.Delegates
+
 class Heap(private var maxsize: Int) {
     private var size: Int = 0
     private var arr: IntArray = IntArray(maxsize)
@@ -7,6 +9,7 @@ class Heap(private var maxsize: Int) {
     private fun parent(i: Int): Int = (i-1)/2
     private fun left(i: Int): Int = 2*i +1
     private fun right(i: Int): Int = 2*i + 2
+    fun peek(): Int = arr[0]
     fun getSize() = size
 
     fun insert(_key: Int) {
@@ -25,6 +28,46 @@ class Heap(private var maxsize: Int) {
         }
 
     }
+
+    private fun extractMax(): Int {
+        val value: Int = arr[0]
+        arr[0] = arr[size--]
+        maxHeapify(0)
+        return value
+    }
+
+    private fun maxHeapify(pos: Int) {
+        if(isLeaf(pos)) {
+            return
+        }
+        if(arr[pos] < arr[left(pos)] || arr[pos] < arr[right(pos)]) {
+            if(arr[left(pos)] > arr[right(pos)]) {
+                swap(pos, left(pos))
+                maxHeapify(left(pos))
+            } else {
+                swap(pos, right(pos))
+                maxHeapify(right(pos))
+            }
+
+        }
+    }
+
+    fun delete(_i: Int) {
+        var i = _i
+        arr[i] = Int.MAX_VALUE
+        while (i != 0 && arr[parent(i)] < arr[i]) {
+            swap(i, parent(i))
+            i = parent(i)
+        }
+        extractMax()
+    }
+
+    private fun isLeaf(pos: Int): Boolean {
+        var bool by Delegates.notNull<Boolean>()
+        bool = pos > maxsize/2 && pos <= maxsize
+        return bool
+    }
+
 
     private fun swap(i: Int, parent: Int) {
         val temp = arr[i]
@@ -50,6 +93,10 @@ fun main() {
     ll.insert(8)
     ll.insert(6)
     ll.printHeap()
-    println(ll.getSize())
+    println()
+    ll.delete(4)
+    ll.printHeap()
 
+    println(ll.peek())
+    println(ll.getSize())
 }
