5e8c04a173fe022a915ce17d44b45fc332a01d5f
Marko Djukic <marko.djukic.111@gmail.com>
2021-01-14 18:21:54 +0100

    --no-message

--- src/datastructures/Heap.kt
+++ src/datastructures/Heap.kt
@@ -2,9 +2,9 @@ package datastructures
 
 import kotlin.properties.Delegates
 
-class Heap(private var maxsize: Int) {
+class Heap {
     private var size: Int = 0
-    private var arr: IntArray = IntArray(maxsize)
+    private var arr: MutableList<Int> = mutableListOf()
 
     private fun parent(i: Int): Int = (i-1)/2
     private fun left(i: Int): Int = 2*i +1
@@ -15,45 +15,41 @@ class Heap(private var maxsize: Int) {
     fun insert(_key: Int) {
         var i = size
 
-        if(size < maxsize) {
-            arr[i] = _key
-            size++
+        arr.add(i, _key)
+        size++
 
-            while (i != 0 && arr[parent(i)] < arr[i]) {
-                swap(i, parent(i))
-                i = parent(i)
-            }
-        } else {
-            println("Heap is full!")
+        while (i != 0 && arr[parent(i)] < arr[i]) {
+            swap(i, parent(i))
+            i = parent(i)
         }
 
     }
 
     private fun extractMax(): Int {
         val value: Int = arr[0]
-        arr[0] = arr[size--]
+        arr[0] = arr[size - 1]
+        arr.removeAt(size - 1)
+        size--
         maxHeapify(0)
         return value
     }
 
     private fun maxHeapify(pos: Int) {
-        if(isLeaf(pos)) {
-            return
-        }
-        if(arr[pos] < arr[left(pos)] || arr[pos] < arr[right(pos)]) {
-            if(arr[left(pos)] > arr[right(pos)]) {
-                swap(pos, left(pos))
-                maxHeapify(left(pos))
-            } else {
-                swap(pos, right(pos))
-                maxHeapify(right(pos))
+        if(!isLeaf(pos) && left(pos) < size && right(pos) < size) {
+            if(arr[pos] < arr[left(pos)] || arr[pos] < arr[right(pos)]) {
+                if(arr[left(pos)] > arr[right(pos)]) {
+                    swap(pos, left(pos))
+                    maxHeapify(left(pos))
+                } else {
+                    swap(pos, right(pos))
+                    maxHeapify(right(pos))
+                }
             }
-
         }
     }
 
     fun delete(_i: Int) {
-        var i = _i
+        var i: Int = _i
         arr[i] = Int.MAX_VALUE
         while (i != 0 && arr[parent(i)] < arr[i]) {
             swap(i, parent(i))
@@ -64,7 +60,7 @@ class Heap(private var maxsize: Int) {
 
     private fun isLeaf(pos: Int): Boolean {
         var bool by Delegates.notNull<Boolean>()
-        bool = pos > maxsize/2 && pos <= maxsize
+        bool = pos > size/2 && pos <= size
         return bool
     }
 
@@ -77,7 +73,7 @@ class Heap(private var maxsize: Int) {
 
     fun printHeap() {
         for(i in arr) {
-            println("($i , size: $size, maxsize: $maxsize)")
+            println("($i , size: $size)")
         }
     }
 
@@ -85,18 +81,23 @@ class Heap(private var maxsize: Int) {
 }
 
 fun main() {
-    val ll = Heap(7)
+    val ll = Heap()
     ll.insert(5)
-    ll.insert(2)
-    ll.insert(1)
     ll.insert(9)
-    ll.insert(8)
-    ll.insert(6)
+    ll.insert(11)
+    ll.insert(14)
+    ll.insert(18)
+    ll.insert(19)
+    ll.insert(21)
+    ll.insert(33)
+    ll.insert(17)
+    ll.insert(27)
     ll.printHeap()
     println()
-    ll.delete(4)
+    ll.delete(1)
     ll.printHeap()
 
     println(ll.peek())
     println(ll.getSize())
+
 }
